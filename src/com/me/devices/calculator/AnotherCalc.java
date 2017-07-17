548d1bd8d049c5d63a3aaf036e28a6ebcc53dd86
Rick Buitrago <rbuitragoc@gmail.com>
2017-07-16 22:12:38 -0500

    added non-static way to translate into functional interface

--- src/com/me/devices/calculator/AnotherCalc.java
+++ src/com/me/devices/calculator/AnotherCalc.java
@@ -5,5 +5,13 @@ public class AnotherCalc {
     public static double roundDecimals(double x, double y) {
         return Math.rint((y != 0? x / y: 0) * 100) / 100;
     }
+    
+    private double defaultAlphaValue = 100;
+    
+    private double defaultBetaValue = 200;
+    
+    public double roundDecimalsWithDefaults(double x, double y) {
+        return x == 0 && y == 0? roundDecimals(defaultAlphaValue, defaultBetaValue): roundDecimals(x, y);
+    }
 
 }
--- test/com/me/devices/calculator/AnotherCalcTest.java
+++ test/com/me/devices/calculator/AnotherCalcTest.java
@@ -17,5 +17,20 @@ public class AnotherCalcTest {
         double rounded = Calculator.executeOperation(j, k, rounding);
         assertTrue(AnotherCalc.roundDecimals(j, k) == rounded);
     }
+    
+    @Test
+    public void testRoundDecimalsDirectly() {
+        double rounded = Calculator.executeOperation(j, k, AnotherCalc::roundDecimals);
+        double expected = AnotherCalc.roundDecimals(j, k);
+        assertTrue(expected == rounded);
+    }
+    
+    @Test
+    public void testRoundDecimalsNonStatic () {
+        // I'll also use the defaults
+        double rounded = Calculator.executeOperation(0,  0, new AnotherCalc()::roundDecimalsWithDefaults);
+        double expected = AnotherCalc.roundDecimals(100, 200);
+        assertTrue(expected == rounded);
+    }
 
 }
